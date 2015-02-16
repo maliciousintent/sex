b8e03523198b273d8738788b32344b62a6d9e09f
Simone Lusenti <simone@plasticpanda.com>
2015-02-16 15:41:02 +0100

    handle errors & default filename

--- sex.sh
+++ sex.sh
@@ -4,6 +4,17 @@
 # Usage: sex .env-production
 #
 
+name=$1
+
+if [ ! -s "$1" ]; then
+	name=".env"
+fi
+
+if [ ! -f "$name" ]; then
+	echo "No such file: $name"
+	exit 1
+fi
+
 for line in $(cat $1); do
 	export $line
 done
